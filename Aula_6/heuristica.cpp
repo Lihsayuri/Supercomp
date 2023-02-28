5649ca85125e956e0454846305838d7a88ed3df6
monstrinho <liviasm1@al.insper.edu.br>
2023-02-28 17:49:49 +0000

    Aula6 feita, fazer de outro jeitoo!

--- Aula_6/heuristica.cpp
+++ Aula_6/heuristica.cpp
@@ -4,6 +4,7 @@
 #include <algorithm> 
 #include <random>
 #include <chrono>
+#include <stdlib.h> 
 // using std::vector
 
 struct item{
@@ -18,70 +19,14 @@ void mais_caro(std::vector<item>&vec, int &n, double &capacidade){
 	});
 }
 
-
-void mais_leve(){
-	std::vector<item>vec;
-	int n;
-	double capacidade;
-
-	std::cin >> n;
-	std::cin >> capacidade;
-
-	for (int i = 0; i < n; i++){
-		item it;
-		it.id = i;
-		std::cin >> it.peso;
-		std::cin >> it.valor;
-		vec.push_back(it);
-	}
-
-	std::sort(vec.begin(), vec.end(), [] (const item &a, const item &b){
-		return a.peso < b.peso;
-	});
-
-
-
-	double peso_final = 0;
-	double valor_final = 0;
-	std::vector<int>resposta(n, 0.0);
-
-	int j = 0;
-	for(int i =0; i < n; i++){
-		if ((peso_final + vec[i].peso) <= capacidade){
-			resposta[j] = vec[i].id;
-			peso_final += vec[i].peso;
-			valor_final += vec[i].valor;
-			j++;
-		}
-		else{
-			break;
-		}
-	}
-
-	resposta.resize(j);
-
-	std::sort(resposta.begin(), resposta.end(), [] (const int &a, const int &b){
-		return a < b;
-	});
-
-	std::cout << peso_final << " " << valor_final << " " << "0"  << "\n";
-
-	for (int i = 0; i < j; i++){
-		std::cout << resposta[i] << " ";
-	}
-}
-
-
-
 int main(){
-
 	std::vector<item>vec;
 	int n;
 	double capacidade;
-	std::vector<int>resposta(n, 0.0);
 
 	std::cin >> n;
 	std::cin >> capacidade;
+	std::vector<int>resposta(n, 0.0);
 
 	for (int i = 0; i < n; i++){
 		item it;
@@ -95,31 +40,30 @@ int main(){
 	//int seed = 10;
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
  	std::default_random_engine generator(seed); 	// aplique uma binomial com n = n e p = 0.75
-	std::binomial_distribution<int> distribution(1,0.75);
-	int sorteio = distribution(generator); // gera número
+	std::binomial_distribution<int> distribution(1, 0.75);
 
-	if (sorteio == 1){
-		for (int i = 0; i < n; i++){
-			mais_caro(vec, n, capacidade, resposta);
-			repo
-			
-		}
-
+	for (int i = 0; i < n; i++){
+		int heuristica = distribution(generator)*abs(rand()%2);
+		resposta[i] = heuristica;
 	}
+
+	unsigned seed2 = std::chrono::system_clock::now().time_since_epoch().count();
+ 	std::default_random_engine generator2(seed2); 	// aplique uma binomial com n = n e p = 0.75
+	std::binomial_distribution<int> distribution2(1,0.25);
 	
 
+	for (int i = 0; i < n; i++){
+		int sorteio = distribution(generator2)*abs(rand()%2); // gera número
+		if ((sorteio == 1) && (resposta[i] == 0)){
+			resposta[i] = sorteio;
+		} else if ((sorteio == 0) && (resposta[i] == 1)){
+			resposta[i] = sorteio;
+		}
+	}
 
-	std::cout << "Sorteio " <<  sorteio ;
-
-
-
-	// mais_caro(vec, n, capacidade);
-
-
-
-	// std::cout << "Agora o mais leve: \n";
-	// mais_leve();
-
+	for (int i = 0; i < n; i++){
+		std::cout << resposta[i] << " ";
+	}
 
 
 }
