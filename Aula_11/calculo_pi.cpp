8f6deeb9c5fa8f46e12794535566f9131ca24314
monstrinho <liviasm1@al.insper.edu.br>
2023-04-11 14:19:13 +0000

    Aula11 feita no monstrao!

---
+++ Aula_11/calculo_pi.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <unistd.h>
+#include <omp.h>
+#include <chrono>
+
+double calcula_pi(int n){
+    double soma = 0;
+    int multiplicador = -1;
+    #pragma omp parallel
+    for (int i = 0; i < n; i++){
+        //soma+= pow(-1, i)/(2*i + 1);// para que ficar fazendo pow toda hora se é só fazer - par 1 ou - impar -1
+        multiplicador *= -1;
+        soma+= (double) multiplicador/(2*i + 1);  // fazer isso pra unidade de processamento não utilizar o ponto flutuante
+
+        // ouuuu
+        // if (i%2 == 0){
+        //     soma+= 1/(2*i + 1);
+        // }else{
+        //     soma+= -1/(2*i + 1);
+        // }
+
+    }
+
+    return soma*4;
+}
+
+int main(){
+    auto t_start = std::chrono::high_resolution_clock::now();
+    long int n = 10000000000;
+    double pi = calcula_pi(n);
+    std::cout << "Pi = " << pi << std::endl;
+
+    auto t_end = std::chrono::high_resolution_clock::now();
+
+    double elapsed_time_ms = std::chrono::duration<double, std::milli>(t_end-t_start).count();
+    double elapsed_time_s = elapsed_time_ms/1000;
+    std::cout << "Tempo de execução: " << elapsed_time_s << " s" << std::endl;
+
+
+    return 0;
+}
